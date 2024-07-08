bbc44ee5241b741bfa1e158d6985128945e0b4be
Malachi <ukomalachi@gmail.com>
2024-07-09 00:35:19 +0100

    use connection string in pg

--- database/index.js
+++ database/index.js
@@ -7,14 +7,10 @@ const { Client } = require("pg")
 async function connectToDatabase() {
     try {
         console.log(`CONNECTING TO DATABASE...`)
-        const client = new Client({
-            host: "dpg-cq6700rv2p9s73cgvbj0-a",
-            user: "hngtask2user",
-            password: "RdGhrp39iHegj6150uqFx7zyYkWVBCXz",
-            port: 5432,
-            database: "hngtask2db"
+        let  client = new Client({
+            connectionString: process.env.PG_CONN_STRING
         })
-        await client.connect();
+        client = await client.connect();
         client.on("error", err => {
             if (err) process.exit(-1);
         })
